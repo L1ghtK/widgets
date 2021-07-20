6c7f4385487ff82b62c2e8b80ae7f18a7866a2d8
kesult <kesult@taltech.ee>
2021-07-20 09:20:55 +0300

    Widgets initial

---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+const Dropdown = ({ options, selected, onSelect, label }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    const renderedOptions = options.filter((option) => option !== selected).map((option) => {
+        return (
+            <div key={option.value} className={"item"} onClick={() => onSelect(option)}>
+                {option.label}
+            </div>
+        );
+    });
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+
+        return () => {
+            document.body.removeEventListener("click", onBodyClick, {
+                capture: true,
+            });
+        };
+    }, []);
+
+    return (
+        <div ref={ref} className={"ui form"}>
+            <div className={"field"}>
+                <label className={"label"}>{label}</label>
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${open ? "visible active" : ''}`}>
+                    <i className={"dropdown icon"} />
+                    <div className={"text"}>{selected.label}</div>
+                    <div className={`menu ${open ? "visible transition" : ''}`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dropdown;
